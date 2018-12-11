bdb218a528e334e2e5ba2032b677a20389c6f835
bakerj1988 <44517395+bakerj1988@users.noreply.github.com>
2018-12-11 16:48:28 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 var Discord = require('discord.io');
 var logger = require('winston');
-var auth = require('NTIyMDYyNjMzODk4MDgyMzA0.DvG29w.-ifzpjwbIMXXw1lcUaWsMKy8bXU');
+var auth = require('./auth.json');
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(logger.transports.Console, {
