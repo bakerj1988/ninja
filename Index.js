442cf6e843a0f4389e98b2323271d327ea5a9fb8
bakerj1988 <44517395+bakerj1988@users.noreply.github.com>
2018-12-13 09:52:23 -0600

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,1 +1,9 @@
-
+var userManager = discord.GetUserManager();
+userManager.GetCurrentUser((result, currentUser) =>
+{
+  if (result == Discord.Result.OK)
+  {
+    Console.WriteLine(currentUser.username);
+    Console.WriteLine(522697131249893377);
+  }
+});
